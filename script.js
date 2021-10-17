048507c2b236ee0b7a495f78fe669889bbff0f3a
Justin Xia <justinxia9@gmail.com>
2021-10-17 15:22:19 -0400

    Add light/dark mode toggle function

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const hamburger = document.querySelector('.hamburger');
 const xButton = document.querySelector('.closebtn');
 const links = document.querySelectorAll('.link');
 const overlay = document.querySelector('.overlay');
+const toggleSwitch = document.querySelector('.round');
+let dark = true;
+const text = [document.querySelector('.main-heading'), document.querySelector('.name'), document.querySelector('.title'),
+document.querySelector('.resume'), document.querySelector('.toggle-icon'), document.querySelector('.sun'), hamburger];
 
 
 function open() {
@@ -13,10 +17,20 @@ function close() {
 }
 
 function toggle() {
-  hamburger.style.color = '#000';
-  document.body.style.backgroundColor = '#fff';
+  dark = !dark;
+  console.log(dark);
+  if(!dark){
+    text.forEach(item => item.style.color = "black");
+    document.body.style.backgroundColor = "white";
+  } else {
+    text.forEach(item => item.style.color = "white");
+    document.body.style.backgroundColor = "black";
+  }
 }
 
 hamburger.addEventListener('click', open);
 xButton.addEventListener('click', close);
 links.forEach(link => link.addEventListener('click', close));
+toggleSwitch.addEventListener('click', toggle);
+
+// white to black: hamburger, overlay, links, .main-heading, .name, .title, .resume, .toggle-icon
