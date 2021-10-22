69f6fc6910d04c1f4fdd40dc0b06b9cdb515d394
Justin Xia <justinxia9@gmail.com>
2021-10-22 11:21:03 -0400

    Complete light mode for intro section

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const overlay = document.querySelector(".overlay");
 const headings = Array.from(document.querySelectorAll("h1"));
 const toggleSwitch = document.querySelector(".round");
 let dark = true;
+const icons = document.querySelectorAll(".icon");
 const text = [document.querySelector(".name"), document.querySelector(".title"),
 document.querySelector(".resume"), document.querySelector(".toggle-icon"), document.querySelector(".sun"), hamburger];
 
@@ -20,15 +21,20 @@ function close() {
 function toggle() {
   dark = !dark;
   if(!dark){
+    document.body.style.backgroundColor = "#fff";
     overlay.style.backgroundColor = "#000";
     links.forEach(link => link.style.color = "#fff");
+    xButton.style.color = "#fff";
+    document.querySelector(".resume").style.border = "1px solid #000";
+
     text.forEach(item => item.classList.add("lightmode"));
     headings.forEach(heading => heading.classList.add("lightmode"));
-    document.body.style.backgroundColor = "white";
+
   } else {
+    document.body.style.backgroundColor = "black";
+    document.querySelector(".resume").style.border = "1px solid #fff";
     text.forEach(item => item.classList.remove("lightmode"));
     headings.forEach(heading => heading.classList.remove("lightmode"));
-    document.body.style.backgroundColor = "black";
   }
 }
 
